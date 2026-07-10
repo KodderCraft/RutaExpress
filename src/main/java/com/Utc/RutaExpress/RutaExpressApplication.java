f34c2e5fa03362a0748f8a70bcd2e740e1ccff1e
Kooder <codygaamez@gmail.com>
2026-07-10 14:05:04 -0500

    Inicio del Proyecto

---
+++ src/main/java/com/Utc/RutaExpress/RutaExpressApplication.java
@@ -0,0 +1,13 @@
+package com.Utc.RutaExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Utc/RutaExpress/RutaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Utc.RutaExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
