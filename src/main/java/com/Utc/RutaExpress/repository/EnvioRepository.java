9fa78dc6169840d7993b5ab10d00bb6ba44f9846
JulianaEstrada1424 <juliana.estrada0746@utc.edu.ec>
2026-07-18 20:58:13 -0500

    Resolver errores en EnvioRepository

--- src/main/java/com/Utc/RutaExpress/repository/EnvioRepository.java
+++ src/main/java/com/Utc/RutaExpress/repository/EnvioRepository.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 import com.Utc.RutaExpress.entity.Envio;
 
 @Repository
 public interface EnvioRepository extends JpaRepository<Envio, Long> {
-<<<<<<< HEAD
     List<Envio> findByDestinatarioId(Long destinatarioId);
-=======
+    List<Envio> findByRemitenteId(Long remitenteId);
 
-List<Envio> findByRemitenteId(Long remitenteId);
-List<Envio> findByDestinatarioId(Long destinatarioId);
-
->>>>>>> origin/funcionalidades
 }
